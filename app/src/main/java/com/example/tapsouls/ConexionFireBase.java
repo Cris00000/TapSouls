3f231b6146a31355a819b0085ec71e129ed3a835
Cristina <cristina.guisado@campusfp.es>
2022-12-14 14:02:14 +0100

    Conexión iniciada

--- app/src/main/java/com/example/tapsouls/ConexionFireBase.java
+++ app/src/main/java/com/example/tapsouls/ConexionFireBase.java
@@ -1,4 +1,15 @@
 package com.example.tapsouls;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class ConexionFireBase {
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
+    public void conectar(){
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Jugadores");
+    }
+
 }
--- app/src/main/java/com/example/tapsouls/RegistroJuego.java
+++ app/src/main/java/com/example/tapsouls/RegistroJuego.java
@@ -3,12 +3,33 @@ package com.example.tapsouls;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegistroJuego extends AppCompatActivity {
+
+    Jugador jugador;
+    ConexionFireBase conexion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_juego);
+
+        conexion.conectar();
+
+        EditText inputCorreo = findViewById(R.id.introducirCorreo);
+        EditText inputUsuario = findViewById(R.id.introducirCorreo);
+        EditText inputCorreo = findViewById(R.id.introducirCorreo);
+
+        Button botonRegistro = (Button) findViewById(R.id.botonRegistrarse);
+
+        botonRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String correo =
+            }
+        });
     }
 }
